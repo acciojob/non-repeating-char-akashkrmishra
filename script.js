388f8f2521c98699a0200b81e798e45e37d63610
akashkrmishra <142215838+akashkrmishra@users.noreply.github.com>
2024-06-17 00:57:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for(let i=0; i<str.length;i++){
+		let char=str.charAt(i);
+		if(str.match(i)){
+			i++;
+		}else{
+			return char;
+		}
+	}
+	return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
